deb22bf79ea0ae2b592d5352dac0864af3e2f6ec
kilocharlie96 <cernak.kiko@gmail.com>
2023-12-03 23:04:44 +0100

    Icons order changed, jquery sample added

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var pageWidth = $('body').width(),
+    h1 = $('header h1'),
+    h2 = $('header h4'),
+    content = $('.content');
+
+    h1.hide().fadeIn(2000);
+    h2.hide().fadeIn(3000);
+    content.hide().fadeIn(3500);
+
