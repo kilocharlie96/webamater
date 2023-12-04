51fbfd2bd217b97f48e58450a4fd529051e21267
kilocharlie96 <cernak.kiko@gmail.com>
2023-12-05 00:42:34 +0100

    Dorobena funkcionalita pre schovanie elementu s varovanim

--- js/script.js
+++ js/script.js
@@ -1,8 +1,16 @@
 var pageWidth = $('body').width(),
     h1 = $('header h1'),
     h2 = $('header h4'),
-    content = $('.content');
+    content = $('.content'),
+    agree = $('.agree');
 
     h1.hide().fadeIn(2000);
     h2.hide().fadeIn(4500);
     content.hide().fadeIn(3000);
+
+    // schova p.warning po kliknuti na agree element
+        agree.on( "click", function() {
+        $('.warning').animate({
+            left: pageWidth
+        }, 1500).fadeOut();
+      } );
