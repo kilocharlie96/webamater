425233e20a9fa871b5dad4474d7c7ef852a45e3d
kilocharlie96 <cernak.kiko@gmail.com>
2023-12-05 09:22:39 +0100

    Changed warning banner behavior after clicking agree button

--- js/script.js
+++ js/script.js
@@ -2,16 +2,18 @@ var pageWidth = $('body').width(),
     h1 = $('header h1'),
     h2 = $('header h4'),
     content = $('.content'),
-    agree = $('.agree');
+    agreeButton = $('.agree'),
+    warningButton = $('.warning')
 
     h1.hide().fadeIn(2000);
     h2.hide().fadeIn(4500);
     content.hide().fadeIn(3000);
 
-    // schova p.warning po kliknuti na agree element
+    // schova warning banner po kliknuti na agree
     // todo dohladat ako schovať element už počas presuvania
-        agree.on( "click", function() {
+        agreeButton.on( "click", function() {
         $('.warning').animate({
-            left: pageWidth
+            left: pageWidth/2,
+            opacity: 0
         }, 1000).fadeOut();
       } );
