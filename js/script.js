fc5d11430ab2ec482f533d4ddc0d2ba3ee5079fc
kilocharlie96 <cernak.kiko@gmail.com>
2025-10-07 22:47:33 +0200

    Refactor jquery to make fadeIn work again

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
-var pageWidth = $('body').width(),
-    h1 = $('header h1'),
-    h2 = $('header h4'),
-    content = $('main'),
-    agreeButton = $('button.button'),
-    warningBanner = $('.warning')
+$(document).ready(function() {
+  var pageWidth = $('body').width(),
+      h1 = $('header h1'),
+      h4 = $('header h4'),
+      content = $('main'),
+      agreeButton = $('button.button'),
+      warningBanner = $('.warning');
 
-    h1.hide().fadeIn(1000);
-    h2.hide().fadeIn(3500);
-    content.hide().fadeIn(2000);
+  h1.hide().fadeIn(1500);
+  h4.hide().fadeIn(3500);
+  content.hide().fadeIn(2000);
 
-    // schova warning banner po kliknuti na agree
-    // todo dohladat ako schovať element už počas presuvania
-        agreeButton.on( "click", function() {
-        warningBanner.animate({
-            left: pageWidth/2,
-            opacity: 0
-        }, 500).fadeOut();
-      } );
+  agreeButton.on("click", function() {
+    warningBanner.animate({
+        left: pageWidth/2,
+        opacity: 0
+    }, 500).fadeOut();
+  });
+});
