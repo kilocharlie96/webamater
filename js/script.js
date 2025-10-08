84ed45a24dffafac52869714b8452cefc0c70009
kilocharlie96 <cernak.kiko@gmail.com>
2025-10-08 13:29:52 +0200

    Animate header, only when DOM is loaded.

--- js/script.js
+++ js/script.js
@@ -1,19 +1,35 @@
-jQuery(function() {
+// Run when the DOM is fully loaded
+$(document).ready(function() {
   var pageWidth = $('body').width(),
-      h1 = $('header h1'),
-      h4 = $('header h4'),
-      content = $('main'),
       agreeButton = $('button.button'),
       warningBanner = $('.warning');
 
-  h1.hide().fadeIn(1500);
-  h4.hide().fadeIn(3500);
-  content.hide().fadeIn(2000);
+  // Trigger animation for elements already in the DOM
+  animateHeader();
 
+  // Hide the warning banner when the agree button is clicked
   agreeButton.on("click", function() {
     warningBanner.animate({
-        left: pageWidth/2,
-        opacity: 0
+      left: pageWidth / 2,
+      opacity: 0
     }, 500).fadeOut();
   });
- })
+
+  // Listen for HTMX content swaps
+  // This fires after HTMX inserts new HTML into the DOM
+  document.body.addEventListener('htmx:afterSwap', function(event) {
+    if (event.target.tagName.toLowerCase() === 'header') {
+      animateHeader();
+    }
+  });
+
+  // Animate the header elements (fade-in effect)
+  function animateHeader() {
+    var h1 = $('header h1');
+    var h4 = $('header h4');
+    if (h1.length && h4.length) {
+      h1.hide().fadeIn(1500);
+      h4.hide().fadeIn(3500);
+    }
+  }
+});
