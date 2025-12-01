361c0372133db217c4f3a868538cf1eab2a68a30
kilocharlie96 <cernak.kiko@gmail.com>
2025-12-01 06:41:26 +0100

    Delete comments from script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,6 @@
-// Run when the DOM is fully loaded
 $(document).ready(function() {
   var pageWidth = $('body').width();
 
-// Use delegated event handler so it works even after HTMX swaps
   $(document).on("click", "button.button", function() {
     var warningBanner = $('.warning');
 
@@ -12,17 +10,14 @@ $(document).ready(function() {
     }, 500).fadeOut();
   });
 
-// Trigger header animation on initial load
   animateHeader();
 
-// Re-run header animation after HTMX replaces the header
   document.body.addEventListener('htmx:afterSwap', function(event) {
     if (event.target.tagName.toLowerCase() === 'header') {
       animateHeader();
     }
   });
 
-// Function to animate header elements (simple fade-in effect)
   function animateHeader() {
     var h1 = $('header h1'),
         h2 = $('header h2'),
